42bf3b146827a65f3e292cae7a88ee57ca710202
youssef18 <youssefodada11@gmail.com>
2026-04-05 23:34:23 +0100

    creating the logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./style.css";
 
-const btnCreatNewTask = document.querySelector(".btnSidebar");
-const listOfTasks = document.querySelector(".contentTaskes");
-const formList = document.querySelector(".form");
-const submitForm = document.querySelector("#submit");
-
-function showForm(){
-  formList.classList.add("show");
-}
-function hideForm(){
-  formList.classList.add("hide");
-}
-btnCreatNewTask.addEventListener("click",showForm);
-submitForm.addEventListener("click",hideForm);
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
+//the logic of open closeing form
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden"),
+);
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+cancelBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+});
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+//creating the rray where to store and display taskes
+const taskData = [];
+let currentTask = {};
+//pervent taskform from reffreching
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`, //so basclly i did this to genertate good id instade of usiln inerval().
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+    <div class="task" id="${id}">
+      <p><strong>Title:</strong>${title}</p>
+      <p><strong>Date:</strong>${date}</p>
+      <p><strong>Description:</strong>${description}</p>
+    </div>`;
+  });
+});
